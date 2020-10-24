43a22010cdc3092afe9625bb6d01a2bffc0619c0
Danil Yasakov <yasakovdanil@gmail.com>
2020-10-24 20:12:51 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    let confArr = bracketsConfig.map(item => item.join(''));
+
+    const strLength = str.length / 2;
+    
+    for (let i = 0; i < strLength; i++) {
+        for (let j = 0; j < confArr.length; j++) {
+            str = str.replace(confArr[j], '');
+        }
+    } 
+    
+    return str.length === 0;
 }
